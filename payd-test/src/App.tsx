b9a1f21684134a8f61b23af33773f94a6a435ad3
Salim Mwatsefu Mwarika <sjmwatsefu@gmail.com>
2024-06-09 13:50:30 +0300

    setup repository

---
+++ payd-test/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className=" bg-red-500">
+      <h1 className=' font-bold'>Hey</h1>
+    </div>
+  );
+}
+
+export default App;
